4f021bdc5798763e241fbd12f39d0a1b8a8f63cc
FRDev66 <frdev66@amp66.fr>
2021-01-31 22:43:17 +0100

    Correction ANO - 0000020: MAIL RAPPORT TOUR - Lien vers la page de téléchargement Rapport erroné
    Signed-off-by: FRDev66 <frdev66@amp66.fr>

--- zIgzAg/jeu/dao2/EnvoyerRapport.java
+++ zIgzAg/jeu/dao2/EnvoyerRapport.java
@@ -48,7 +48,7 @@ public class EnvoyerRapport {
    else m=new MessageFormat(Univers.getMessageInfo("MAIL_CORPS_RAPPORT",c.getLocale()));
   //Object[] o2={c.getLogin(),c.getMotDePasse(),Const.ADRESSE_SITE_RAPPORTS+Integer.toString(c.getNumero())+"/"+Integer.toString(c.getNumero())+".zip"};
   //Object[] o2={c.getLogin(),c.getMotDePasse(),Const.ADRESSE_SITE_RAPPORTS+Integer.toString(c.getNumero())+"/rapports.php"};
-  Object[] o2={c.getLogin(),c.getMotDePasse(),Const.ADRESSE_SITE_RAPPORTS+"/rapports.php"};
+  Object[] o2={c.getLogin(),c.getMotDePasse(),Const.ADRESSE_SITE_RAPPORTS+"rapports.php"};
   String corpsMessage=m.format(o2);
   String[] fichiers=new String[0];
 
